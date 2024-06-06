e0568da486e5ed5ca261912d64693563854a18c0
Team_2 <gafarova.viktoriya@mail.ru>
2024-06-06 20:20:43 +0500

    add new commit var8+

--- var8.py
+++ var8.py
@@ -1,54 +1,55 @@
-# найти количество отдельно мужчин отдельно женщин по каждому классу обслуживания
 import matplotlib.pyplot as plt
 import streamlit as st
 
-def get_pass_count(lines, choise):
+
+def get_pass_count(lines, choice):
     res = {'Мужчин': 0, 'Женщин': 0}
     for line in lines:
         d = line.split(',')
-        Sex = d[5]
-        Pclass = d[2]
-        if choise == "Всего":
-            if Sex in {'Мужчин', 'Женщин'}:
-                res[Sex] += 1
-            if Sex == 'Age':
+        sex = d[5]
+        pclass = d[2]
+        if choice == "Всего":
+            if sex in {'Мужчин', 'Женщин'}:
+                res[sex] += 1
+            if sex == 'Age':
                 continue
-            if Sex == 'male':
+            if sex == 'male':
                 res['Мужчин'] += 1
             else:
                 res['Женщин'] += 1
-        elif choise == "1 класс" and Pclass == '1':
-            if Sex == 'male':
+        elif choice == "1 класс" and pclass == '1':
+            if sex == 'male':
                 res['Мужчин'] += 1
             else:
                 res['Женщин'] += 1
-        elif choise == "2 класс" and Pclass == '2':
-            if Sex == 'male':
+        elif choice == "2 класс" and pclass == '2':
+            if sex == 'male':
                 res['Мужчин'] += 1
             else:
                 res['Женщин'] += 1
-        elif choise == "3 класс" and Pclass == '3':
-            if Sex == 'male':
+        elif choice == "3 класс" and pclass == '3':
+            if sex == 'male':
                 res['Мужчин'] += 1
             else:
                 res['Женщин'] += 1
     return res
+
 
 def do_var8():
     st.title('Вариант 8')
 
     st.header('Данные пассажиров Титаника')
     st.write("Для просмотра данных по количеству пассажиров каждого пола по указанному классу обслуживания, выберите соответствующий пункт из списка")
-    choise = st.selectbox("Укажите класс обслуживания:", ["Всего", "1 класс", "2 класс", "3 класс"])
+    choice = st.selectbox("Укажите класс обслуживания:", ["Всего", "1 класс", "2 класс", "3 класс"])
 
     with open('data.csv') as file:
         lines = file.readlines()
-        # Вызываем функцию get_pass_count с передачей аргумента choise
-        res = get_pass_count(lines, choise)
+        res = get_pass_count(lines, choice)
 
     st.dataframe(res)
-    fig = plt.figure(figsize=(10,5))
-    plt.bar(['Мужчин','Женщин'],[res['Мужчин'],res['Женщин']])
+    fig = plt.figure(figsize=(10, 5))
+    plt.bar(['Мужчин', 'Женщин'], [res['Мужчин'], res['Женщин']])
     st.pyplot(fig)
+
 
 do_var8()
