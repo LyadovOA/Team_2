aa4e3002e707079402b63c22f33244e041c455b2
LyadovOA <168270329+LyadovOA@users.noreply.github.com>
2024-06-11 13:18:48 +0400

    Update var10_test.py

--- var10_test.py
+++ var10_test.py
@@ -1,50 +1,56 @@
 from var10 import get_group_func
 
 
-# Тест для проверки корректного подсчета средней цены для мужчин и женщин
+# №1 Тест для проверки корректного подсчета средней цены для мужчин и женщин
 def test_avg_fares():
     lines = [
-        ",,,,,male,,,,,7.25,,",
-        ",,,,,male,,,,,21.075,,",
-        ",,,,,female,,,,,71.2833,,",
-        ",,,,,female,,,,,7.925,,"
+        "1,0,3,'Braund, Mr. Owen Harris',male,22,1,0,A/5 21171,7.25,,S\n",
+        ("2,1,1,'Cumings, Mrs. John Bradley (Florence Briggs Thayer)',"
+         "female,38,1,0,PC 17599,71.2833,C85,C\n"),
+        "3,1,3,'Heikkinen, Miss. Laina',female,26,0,0,STON/O2. 3101282,7.925,,S\n"
     ]
-    assert get_group_func(lines, 'avg') == (14.1625, 39.60415)
+    avg_fares, _, _ = get_group_func(lines)
+    assert avg_fares == {'male_avg_fare': 7.25, 'female_avg_fare': 39.60415}
 
 
-# Тест для проверки корректного подсчета максимальной цены для мужчин и женщин
+# №2 Тест для проверки корректного подсчета максимальной цены для мужчин и женщин
 def test_max_fares():
     lines = [
-        ",,,,,male,,,,,7.25,,",
-        ",,,,,male,,,,,21.075,,",
-        ",,,,,female,,,,,71.2833,,",
-        ",,,,,female,,,,,7.925,,"
+        "1,0,3,'Braund, Mr. Owen Harris',male,22,1,0,A/5 21171,7.25,,S\n",
+        ("2,1,1,'Cumings, Mrs. John Bradley (Florence Briggs Thayer)',"
+         "female,38,1,0,PC 17599,71.2833,C85,C\n"),
+        "3,1,3,'Heikkinen, Miss. Laina',female,26,0,0,STON/O2. 3101282,7.925,,S\n"
     ]
-    assert get_group_func(lines, 'max') == (21.075, 71.2833)
+    _, _, max_fares = get_group_func(lines)
+    assert max_fares == {'male_max_fare': 7.25, 'female_max_fare': 71.2833}
 
 
-# Тест для проверки корректного подсчета минимальной цены для мужчин и женщин
+# №3 Тест для проверки корректного подсчета минимальной цены для мужчин и женщин
 def test_min_fares():
     lines = [
-        ",,,,,male,,,,,7.25,,",
-        ",,,,,male,,,,,3.15,,",
-        ",,,,,female,,,,,71.2833,,",
-        ",,,,,female,,,,,7.925,,"
+        "1,0,3,'Braund, Mr. Owen Harris',male,22,1,0,A/5 21171,7.25,,S\n",
+        ("2,1,1,'Cumings, Mrs. John Bradley (Florence Briggs Thayer)',"
+         "female,38,1,0,PC 17599,71.2833,C85,C\n"),
+        "3,1,3,'Heikkinen, Miss. Laina',female,26,0,0,STON/O2. 3101282,7.925,,S\n"
     ]
-    assert get_group_func(lines, 'min') == (3.15, 7.925)
+    _, min_fares, _ = get_group_func(lines)
+    assert min_fares == {'male_min_fare': 7.25, 'female_min_fare': 7.925}
 
 
-# Тест для данных с отсутствием информации о ценах билетов для мужчин
+# №4 Тест для данных с отсутствием информации о ценах билетов для мужчин
 def test_no_fares():
     lines = [
-        ",,,,,male,,,,,,,",
-        ",,,,,female,,,,,71.2833,,",
-        ",,,,,female,,,,,7.925,,"
+        "1,0,3,'Braund, Mr. Owen Harris',male,22,1,0,A/5 21171,,S\n",
+        ("2,1,1,'Cumings, Mrs. John Bradley (Florence Briggs Thayer)',"
+         "female,38,1,0,PC 17599,71.2833,C85,C\n"),
+        "3,1,3,'Heikkinen, Miss. Laina',female,26,0,0,STON/O2. 3101282,7.925,,S\n"
     ]
-    assert get_group_func(lines, 'min') == (0, 7.925)
+    _, min_fares, _ = get_group_func(lines)
+    assert min_fares == {'female_min_fare': 7.925, 'male_min_fare': 0}
 
 
-# Тест с полным отсутствием информации
+# №5 Тест с полным отсутствием информации
 def test_no_data():
     lines = []
-    assert get_group_func(lines, 'avg') == (0, 0)
+    avg_fares, _, _ = get_group_func(lines)
+    assert avg_fares == {'male_avg_fare': 0, 'female_avg_fare': 0}
